6fa3e426230740944fde64c9dbc8cbd898036c82
vva07 <vva0779@yandex.ru>
2024-02-12 18:10:17 +0300

    feat(): Lab1 completed

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template_string
+from htmls import result_html, form_html, index_html
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template_string(index_html)
+
+@app.route('/input', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        surname = request.form.get('surname')
+        email = request.form.get('email')
+        return render_template_string(result_html, name=name, surname=surname, email=email)
+    return render_template_string(form_html)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ htmls.py
@@ -0,0 +1,23 @@
+# HTML форма для ввода данных
+form_html = '''
+    <form method="post">
+        Имя: <input type="text" name="name"><br>
+        Фамилия: <input type="text" name="surname"><br>
+        E-mail: <input type="email" name="email"><br>
+        <input type="submit" value="Отправить">
+    </form>
+'''
+
+# HTML шаблон для вывода введенных данных
+result_html = '''
+    <h1>Введенные данные:</h1>
+    Имя: {{ name }}<br>
+    Фамилия: {{ surname }}<br>
+    E-mail: {{ email }}
+'''
+
+# Страница с ссылкой на форму
+index_html = '''
+    <h1>Добро пожаловать!</h1>
+    <a href="/input">Перейти к форме ввода</a>
+'''
