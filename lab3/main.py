48f2a18589cbf8acde6cbddf333543cbc3ff6077
immariav <mariavolynkina03@gmail.com>
2024-02-26 20:34:38 +0300

    feat(): selection of 3 words to show

--- lab3/main.py
+++ lab3/main.py
@@ -2,15 +2,23 @@ from flask import Flask, jsonify, request, render_template_string
 import htmls
 app = Flask(__name__)
 
-# Пример списка слов для автодополнения
-WORDS = ["apple", "banana", "grape", "orange", "mango", "lemon", "banena"]
+def select_top_three_suggestions(suggestions):
+    # Сначала сортируем по длине слова
+    sorted_by_length = sorted(suggestions, key=lambda x: len(x))
+    # Если есть одинаковые по длине слова, сортируем их по алфавиту
+    sorted_alphabetically = sorted(sorted_by_length, key=lambda x: (len(x), x))
+    # Возвращаем только первые три слова (если есть)
+    return sorted_alphabetically[:3]
 
+# Пример списка слов для автодополнения
+WORDS = ["apple", "banana", "grape", "orange", "mango", "lemon", "banena", "baka", "balalaika"]
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     search = request.args.get('term')
     suggestions = [word for word in WORDS if word.startswith(search)] if search else []
-    return jsonify(suggestions)
+    top_three_suggestions = select_top_three_suggestions(suggestions)
+    return jsonify(top_three_suggestions)
 
 
 @app.route('/')
